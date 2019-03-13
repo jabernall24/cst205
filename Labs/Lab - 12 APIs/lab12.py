7e7c3a73ef3d37bc6e8fd14b0705d6849287be9a
jabernall24 <jabernall24@gmail.com>
2019-03-12 19:37:23 -0700

    testing dotenv

--- Labs/Lab - 12 APIs/lab12.py
+++ Labs/Lab - 12 APIs/lab12.py
@@ -1,7 +1,11 @@
-import requests, json
+import requests
 from pprint import pprint
+from dotenv import load_dotenv
+import os
 
-my_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjA1MCwiaWRlbiI6IjQ5MjgyNDgyNTk3OTYwMDg5NyIsIm1kIjp7fSwidHMiOjE1NDMyMTY5Njg3OTV9.okAcprH46D0-MHVmrAatkCifmSAop3i8VK4ZO42sT-w'
+
+load_dotenv()
+my_key = os.getenv("API_KEY")
 
 url = "https://api.royaleapi.com/player/2GUQQ8JGC/chests"
 
