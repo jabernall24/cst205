2f50c267231ab5d0bfb527ae654610016990b835
jabernall24 <jabernall24@gmail.com>
2019-03-12 22:05:57 -0700

    added more to lab 12 and put pdf's for labs 11 and 12

--- Labs/Lab - 12 APIs/lab12.py
+++ Labs/Lab - 12 APIs/lab12.py
@@ -1,19 +1,64 @@
+"""
+Contributers: Jesus Andres Bernal Lopez, Paul Whipp, Michael Avalos-Garcia
+File: lab12.py
+Date: 03/13/2019
+Description:
+"""
+
 import requests
-from pprint import pprint
 from dotenv import load_dotenv
 import os
+from PyQt5.QtWidgets import QWidget, QApplication, QVBoxLayout, QLabel, QPushButton, QLineEdit
+from PyQt5.QtCore import pyqtSlot
+import sys
 
 
-load_dotenv()
-my_key = os.getenv("API_KEY")
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Clash Royale Chest Cycle")
 
-url = "https://api.royaleapi.com/player/2GUQQ8JGC/chests"
+        vbox = QVBoxLayout()
 
-headers = {
-    'auth': my_key
-}
+        label = QLabel("Enter your player tag(without the #)")
+        self.user_input = QLineEdit()
+        self.my_button = QPushButton("Get Chest Cycle")
+        self.my_button.clicked.connect(self.button_clicked)
+        self.chest_label = QLabel()
+        self.chest_label.isHidden()
 
-response = requests.request("GET", url, headers=headers)
+        vbox.addWidget(label)
+        vbox.addWidget(self.user_input)
+        vbox.addWidget(self.my_button)
+        vbox.addWidget(self.chest_label)
 
-data = response.json()
-pprint(data)
+        self.setLayout(vbox)
+
+    @pyqtSlot()
+    def button_clicked(self):
+        url = f"https://api.royaleapi.com/player/{self.user_input.text().upper()}/chests"
+
+        headers = {
+            'auth': my_key
+        }
+
+        response = requests.request("GET", url, headers=headers)
+
+        data = response.json()
+        self.chest_label.setText(f"""
+        Mega Lightning: {data['megaLightning']}\n
+        Magical: {data['magical']}\n
+        Legendary: {data['legendary']}\n
+        Epic: {data['epic']}\n
+        Giant: {data['giant']}
+        """)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_win = Window()
+    main_win.show()
+    # loads the .env file and stores the API Key in variable
+    load_dotenv()
+    my_key = os.getenv("API_KEY")
+    sys.exit(app.exec_())
